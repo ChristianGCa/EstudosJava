ae4869fc987592d8802fa69651b1ea447172a415
christian <christian@christian-Nitro>
2024-03-04 20:07:10 -0300

    Iniciando o estudo da aula 3 sobre HashMap e criando o exercício

---
+++ unidade-1/src/com/acme/ExercicioHashMap.java
@@ -0,0 +1,50 @@
+package com.acme;
+
+public class ExercicioHashMap {
+
+    public static void main(String[] args) {
+
+        String texto = "Caneta azul, azul caneta\n" +
+        "Caneta azul tá marcada com minha letra\n" +
+        "Caneta azul, azul caneta\n" +
+        "Caneta azul tá marcada com minha letra\n" +
+        "Todo dia eu viajo\n" +
+        "Com uma azul e uma amarela\n" +
+        "Eu perdi minha caneta\n" +
+        "Quem achou, devolva ela\n" +
+        "Todo dia eu viajo\n" +
+        "Com azul e uma amarela\n" +
+        "Eu perdi minha caneta\n" +
+        "Quem achou devolva ela\n" +
+        "Caneta azul, azul caneta\n" +
+        "Caneta azul tá marcada com minha letra\n" +
+        "Caneta azul, azul caneta\n" +
+        "Caneta azul tá marcada com minha letra\n" +
+        "Caneta azul, azul caneta\n" +
+        "Caneta azul tá marcada com minha letra\n" +
+        "Caneta azul, azul caneta\n" +
+        "Caneta azul tá marcada com minha letra\n" +
+        "Todo dia eu viajo\n" +
+        "Com uma azul e uma amarela\n" +
+        "Perdi minha caneta\n" +
+        "Quem achou, devolva ela\n" +
+        "Todo dia eu viajo\n" +
+        "Com uma azul e uma amarela\n" +
+        "Perdi minha caneta\n" +
+        "Quem achou, devolva ela\n" +
+        "Caneta azul, azul caneta\n" +
+        "Caneta azul tá marcada com minha letra\n" +
+        "Caneta azul, azul caneta\n" +
+        "Caneta azul tá marcada com minha letra\n" +
+        "Caneta azul, azul caneta\n" +
+        "Caneta azul tá marcada com minha letra\n" +
+        "Caneta azul, azul caneta\n" +
+        "Caneta azul tá marcada com minha letra";
+        
+        System.out.println("Caracteres -> " + texto.length());
+        
+        
+        
+    }
+    
+}
