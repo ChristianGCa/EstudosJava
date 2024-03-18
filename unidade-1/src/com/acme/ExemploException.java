abe04b8285885cc9aeef772d52afb93a4ff013ae
christian <christian@christian-Nitro>
2024-03-18 20:39:38 -0300

    Iniciando o estudo de exception em java

---
+++ unidade-1/src/com/acme/ExemploException.java
@@ -0,0 +1,20 @@
+package com.acme;
+
+public class ExemploException {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Início do algoritmo");
+        float a = 10;
+        float b = 20;
+        
+        System.out.println(args[2]);
+        
+        float res = a / b;
+        
+        System.out.println("O resultado é: " + res);
+        
+        System.out.println("FIM");
+    }
+    
+}
