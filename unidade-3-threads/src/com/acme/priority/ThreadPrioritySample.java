182af4df41be1c1556a7807c1f0c76e79c31287e
christian <christian@christian-Nitro>
2024-05-20 20:41:16 -0300

    Criando os arquivos da aula 2 sobre threads

---
+++ unidade-3-threads/src/com/acme/priority/ThreadPrioritySample.java
@@ -0,0 +1,13 @@
+package com.acme.priority;
+
+public class ThreadPrioritySample {
+
+    public static void main(String[] args) {
+        
+        int[] executions = new int[3];
+        
+        Thread t1 = new Task()
+        
+    }
+    
+}
